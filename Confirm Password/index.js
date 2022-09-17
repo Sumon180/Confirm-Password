b0dba16b347a374bfb62a91acf333ebde03585a6
Sumon Hossain <106359888+Sumon180@users.noreply.github.com>
2022-09-17 10:10:22 +0600

    mini project

---
+++ Confirm Password/index.js
@@ -0,0 +1,53 @@
+const pswrd_1 = document.querySelector("#pswrd_1");
+const pswrd_2 = document.querySelector("#pswrd_2");
+const errorText = document.querySelector(".error-text");
+const showBtn = document.querySelector(".show");
+const btn = document.querySelector("button");
+
+function active() {
+  if (pswrd_1.value.length >= 6) {
+    btn.removeAttribute("disabled", "");
+    btn.classList.add("active");
+    pswrd_2.removeAttribute("disabled", "");
+  } else {
+    btn.setAttribute("disabled", "");
+    btn.classList.remove("active");
+    pswrd_2.setAttribute("disabled", "");
+  }
+}
+
+btn.onclick = function () {
+  console.log("hi");
+  if (pswrd_1.value != pswrd_2.value) {
+    errorText.style.display = "block";
+    errorText.classList.remove("matched");
+    errorText.textContent = "Passsword Not Match";
+    return false;
+  } else {
+    errorText.style.display = "block";
+    errorText.classList.add("matched");
+    errorText.textContent = "Confirm, Password matched";
+    return false;
+  }
+};
+
+function active_2() {
+  if (pswrd_2.value != "") {
+    showBtn.style.display = "block";
+    showBtn.onclick = function () {
+      if (pswrd_2.type == "password" && pswrd_2.type == "password") {
+        pswrd_1.type = "text";
+        pswrd_2.type = "text";
+        this.textContent = "HIDE";
+        this.style.color = "black";
+      } else {
+        pswrd_1.type = "password";
+        pswrd_2.type = "password";
+        this.textContent = "SHOW";
+        this.style.color = "green";
+      }
+    };
+  } else {
+    showBtn.style.display = "none";
+  }
+}
